46537bdc76b44363c0ec52cd34a038fe58c3f7ec
Gromak Maxim <maximgromak@gmail.com>
2021-08-28 18:39:00 +1000

    upd: tests

--- src/index.js
+++ src/index.js
@@ -1,38 +1,14 @@
-const character = {
-    name: 'Лучник',
-    type: 'Bowman',
-    health: 50,
-    level: 3,
-    attack: 40,
-    defence: 10,
-    special: [
-        {
-            id: 8,
-            name: 'Двойной выстрел',
-            icon: 'http://...',
-            description: 'Двойной выстрел наносит двойной урон'
-        },
-        {
-            id: 9,
-            name: 'Нокаутирующий удар',
-            icon: 'http://...'
-            // <- обратите внимание, описание "засекречено"
-        }
-    ]
-}
-
-function getFields(obj) {
-    const result = [];
-    obj.special.forEach((elem) => {
-        const {
-            id, name, icon, description = 'Описание недоступно',
-        } = elem;
-        result.push({
-            id, name, icon, description,
-        });
+export default function getFields(obj) {
+  if (arguments.length === 0) return [];
+  if (Object.entries(obj).length === 0) return [];
+  const result = [];
+  obj.special.forEach((elem) => {
+    const {
+      id, name, icon, description = 'Описание недоступно',
+    } = elem;
+    result.push({
+      id, name, icon, description,
     });
-    return result;
+  });
+  return result;
 }
-
-let o = getFields(character);
-console.log(o)
---
+++ src/test/test.js
@@ -0,0 +1,150 @@
+import getFields from '../index.js';
+
+/*
+correct data
+*/
+
+test('should return arr with 3 elem', () => {
+  const input = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const expectedOutput = [
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  const actualOutput = getFields(input);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('should return arr with 2 elem', () => {
+  const input = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 8,
+        name: 'Двойной выстрел',
+        icon: 'http://...',
+        description: 'Двойной выстрел наносит двойной урон',
+      },
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const expectedOutput = [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  const actualOutput = getFields(input);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('should return arr with 1 elem', () => {
+  const input = {
+    name: 'Лучник',
+    type: 'Bowman',
+    health: 50,
+    level: 3,
+    attack: 40,
+    defence: 10,
+    special: [
+      {
+        id: 9,
+        name: 'Нокаутирующий удар',
+        icon: 'http://...',
+      },
+    ],
+  };
+
+  const expectedOutput = [
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+      description: 'Описание недоступно',
+    },
+  ];
+
+  const actualOutput = getFields(input);
+
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+/*
+incorrect data
+*/
+
+test('should return empty arr (given empty obj)', () => {
+  const input = {};
+  const expectedOutput = [];
+  const actualOutput = getFields(input);
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
+
+test('should return empty arr (no args)', () => {
+  const expectedOutput = [];
+  const actualOutput = getFields();
+  expect(actualOutput).toStrictEqual(expectedOutput);
+});
